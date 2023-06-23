08d958fd79a2e8844797b3d03a5c7a95170075e2
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2023-06-22 23:00:16 -0500

    Import bootstrap.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+// Import boostrap.css
+import '@/styles/bootstrap.min.css'
+
 import '@/styles/globals.css'
 
+import Head from 'next/head'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
+
+  <Component {...pageProps} />
+  </>
+  );
 }
